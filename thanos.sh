9e5dfa346110e774589a98639f543f96476d582a
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2020-09-12 16:05:33 -0400

    update thanos.sh

--- thanos.sh
+++ thanos.sh
@@ -1,12 +1,21 @@
 run_thanos(){
-    for i in $(find / -type f 2> /dev/null); do
-        if [ $(($RANDOM % 2)) -eq 1 ]; then
-            rm -rf $i
-            echo "$i was snapped from existence"
-        else
-            echo "$i was spared"
-        fi
-    done
+  export perc=20
+  while true; do
+    export usage=$(df -h / | egrep -o '[0-9]+%' | head -n 1 | sed "s/%//g")
+    if [ "$usage" -ge "$(echo 75)" ] ; then
+      for i in $(find / -type f 2> /dev/null); do
+          if [ $(($RANDOM % 2)) -eq 1 ]; then
+#              rm -rf $i
+              echo "$i was snapped from existence"
+          else
+              echo "$i was spared"
+          fi
+        done
+      fi
+    sleep 300
+  done
 }
 
 nohup run_thanos &
+
+
